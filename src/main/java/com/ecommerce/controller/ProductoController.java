38460374602b25d964a85139330fdaa4f116c318
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-26 20:25:17 -0300

    Creacion de vista edit.html e implementacion de metodo Update en ProductoController

--- src/main/java/com/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/controller/ProductoController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -23,21 +26,39 @@ public class ProductoController {
 
     @GetMapping("")
     public String show(Model model) {
-        model.addAttribute("productos",productoService.findAll());
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
     }
 
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "productos/create";
     }
 
     @PostMapping("/save")
-    public String save(Producto producto){
-        LOGGER.info("Este es el objeto producto {}",producto);
-        Usuario usuario = new Usuario(1,"","","","","","","");
+    public String save(Producto producto) {
+        LOGGER.info("Este es el objeto producto {}", producto);
+        Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
         producto.setUsuario(usuario);
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Producto producto = new Producto();
+        Optional<Producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("Producto buscado: {}", producto);
+        model.addAttribute("producto",producto);
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
 }
