0b8150fc628dccb808a6f266080e8bf63b23fed1
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-30 17:26:40 -0300

    Mostrar productos en usuario/home.html

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.repository.ProductoRepository;
+import com.ecommerce.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+
+
+    @GetMapping("")
+    public String home(Model model) {
+        model.addAttribute("productos",productoService.findAll());
+        return "usuario/home";
+    }
+
+}
