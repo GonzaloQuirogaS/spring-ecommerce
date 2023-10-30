40e07defec8f9044a9e0d2d5b4a57c3d6bc3b72d
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-30 18:02:32 -0300

    Mostrar datos de producto en productohome

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -2,24 +2,34 @@ package com.ecommerce.controller;
 
 import com.ecommerce.repository.ProductoRepository;
 import com.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
     @Autowired
     private ProductoService productoService;
-
 
     @GetMapping("")
     public String home(Model model) {
         model.addAttribute("productos",productoService.findAll());
         return "usuario/home";
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        log.info("Id enviado como parametro: {}",id);
+        return "usuario/productohome";
     }
 
 }
