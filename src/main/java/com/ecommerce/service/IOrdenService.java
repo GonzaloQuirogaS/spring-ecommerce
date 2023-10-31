0b826de85bacb00deaa8890515e34f5277ccfd73
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-31 19:17:15 -0300

    Creacion de metodo para generar numero de orden

--- src/main/java/com/ecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/service/IOrdenService.java
@@ -3,6 +3,9 @@ package com.ecommerce.service;
 
 import com.ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
     Orden save(Orden orden);
+    List<Orden> findAll();
 }
