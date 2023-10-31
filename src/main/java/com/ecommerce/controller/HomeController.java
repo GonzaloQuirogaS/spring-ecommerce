709f935aae6a7ca8ac6e00719ae1a8b2857eb0c8
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-10-30 22:43:11 -0300

    Obtener id y datos de producto en cart

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.model.DetalleOrden;
+import com.ecommerce.model.Orden;
 import com.ecommerce.model.Producto;
 import com.ecommerce.repository.ProductoRepository;
 import com.ecommerce.service.ProductoService;
@@ -8,11 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -24,32 +25,45 @@ public class HomeController {
     @Autowired
     private ProductoService productoService;
 
+    //Almacenar detalles de orden
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    //Datos de orden
+    Orden orden = new Orden();
+
     @GetMapping("")
     public String home(Model model) {
-        model.addAttribute("productos",productoService.findAll());
+        model.addAttribute("productos", productoService.findAll());
         return "usuario/home";
     }
 
     @GetMapping("productohome/{id}")
-    public String productoHome(@PathVariable Integer id, Model model){
+    public String productoHome(@PathVariable Integer id, Model model) {
 
-        log.info("Id enviado como parametro: {}",id);
+        log.info("Id enviado como parametro: {}", id);
         Producto producto = new Producto();
         Optional<Producto> productoOptional = productoService.get(id);
         producto = productoOptional.get();
 
-        model.addAttribute("producto",producto);
+        model.addAttribute("producto", producto);
 
         return "usuario/productohome";
     }
 
     //CARRITO
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        log.info("Producto añadido: {}",optionalProducto.get());
+        log.info("Cantidad: {}",cantidad);
 
         return "usuario/carrito";
     }
-
 
 
 }
