e41636e0502ca222bbb16705da6287aa95e769af
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-11-03 22:13:00 -0300

    Metodo para filtrar productos desde nav

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -168,6 +169,17 @@ public class HomeController {
         detalles.clear();
 
         return "redirect:/";
+    }
+
+    @PostMapping("/search")
+    public String searchProduct(@RequestParam String nombre, Model model) {
+
+        log.info("Nombre del producto: {}", nombre);
+
+        List<Producto> productos = productoService.findAll().stream().filter(p -> p.getNombre().contains(nombre)).collect(Collectors.toList());
+
+        model.addAttribute("productos",productos);
+        return "usuario/home";
     }
 
 
