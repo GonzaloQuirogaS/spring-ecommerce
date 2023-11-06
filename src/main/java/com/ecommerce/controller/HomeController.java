c4af46171f28b44d090f4f3369f52cb924385040
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-11-06 10:06:34 -0300

    Implementar template para usuario y admin

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -51,6 +51,8 @@ public class HomeController {
         log.info("Sesion de usuario: {}", session.getAttribute("idusuario"));
         model.addAttribute("productos", productoService.findAll());
 
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+
         return "usuario/home";
     }
 
@@ -131,10 +133,11 @@ public class HomeController {
     }
 
     @GetMapping("/getCart")
-    public String getCart(Model model) {
+    public String getCart(Model model, HttpSession session) {
 
         model.addAttribute("cart", detalles);
         model.addAttribute("orden", orden);
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
 
         return "usuario/carrito";
     }
