7d055edb30397e20046aca4a5bf657810c39c5c9
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-11-06 10:31:10 -0300

    Crear y mostrar vista de compras para usuario

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,11 +59,18 @@ public class UsuarioController {
             } else {
                 return "redirect:/";
             }
-        }else {
+        } else {
             log.info("Usuario no existente");
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(HttpSession session, Model model) {
+
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+        return "usuario/compras";
     }
 
 }
