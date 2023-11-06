15b1c8e2b04215c926e5c26e5c4ad0229155bc8e
Gonzalo Quiroga <gonzaquirogasassu@gmail.com>
2023-11-06 20:14:19 -0300

    Metodo para cerrar sesion de usuario y de administrador

--- src/main/java/com/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/controller/UsuarioController.java
@@ -97,4 +97,11 @@ public class UsuarioController {
         return "usuario/detallecompra";
     }
 
+    @GetMapping("/cerrar")
+    public String cerrarSesion(HttpSession session){
+        session.removeAttribute("idusuario");
+
+        return "redirect:/";
+    }
+
 }
